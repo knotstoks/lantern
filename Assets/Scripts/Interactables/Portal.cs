4c4aebc666fe77ad33d85aa6855961a1f8539a34
unknown <knotstoks@gmail.com>
2021-05-05 04:34:44 +0800

    Added logic for portals and saving

--- Assets/Scripts/Interactables/Portal.cs
+++ Assets/Scripts/Interactables/Portal.cs
@@ -3,8 +3,10 @@ using UnityEngine.SceneManagement;
 
 public class Portal : Interactable {
     [SerializeField] private string nextScene;
+    [SerializeField] private Vector2 position;
 
     public override void Interact() {
+        DataStorage.position = position;
         SceneManager.LoadScene(nextScene);
     }
 }
