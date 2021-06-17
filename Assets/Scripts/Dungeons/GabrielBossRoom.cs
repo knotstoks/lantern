ebce712973f1c725bb22e1949f4ee135583124fc
knotstoks <70320812+knotstoks@users.noreply.github.com>
2021-06-17 10:28:24 +0800

    Merge for Gabriel Boss Room

---
+++ Assets/Scripts/Dungeons/GabrielBossRoom.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GabrielBossRoom : MonoBehaviour {
+    [SerializeField] private Dialogue[] introDialogues;
+    [SerializeField] private Dialogue outroDialogue;
+    void Start() {
+
+    }
+    void Update() {
+        
+    }
+}
---
+++ Assets/Scripts/EnemyAI/1/Bosses/Gabriel/Gabriel.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gabriel : Boss {
+    public bool start;
+    private void Start() {
+        Initialize();
+        start = false;
+    }
+    private void Update() {
+        
+    }
+}
